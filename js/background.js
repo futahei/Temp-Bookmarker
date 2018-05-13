d6052e29ac24b229bb8e3b7a6d31636f2737f99b
DESKTOP-N7Q5U4L\Futahei <futahei1108@icloud.com>
2018-05-13 20:24:50 +0900

    :rench:

--- js/background.js
+++ js/background.js
@@ -1,1 +1,13 @@
-chrome.browserAction.setBadgeText({text:"ABC"});
+var counter = 0;
+chrome.tabs.getAllInWindow(null, function(tabs){
+  counter = tabs.length;
+  chrome.browserAction.setBadgeText({text:String(counter)});
+});
+chrome.tabs.onCreated.addListener(function(tab){
+  counter++;
+  chrome.browserAction.setBadgeText({text:String(counter)});
+})
+chrome.tabs.onRemoved.addListener(function(tab){
+  counter--;
+  chrome.browserAction.setBadgeText({text:String(counter)});
+})
