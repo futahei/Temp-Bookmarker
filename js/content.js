bdd509837d368ea8be68a9e7f5dbbec3fdbc7433
futahei-surface <smilenanakohei0814@gmail.com>
2018-05-14 19:18:07 +0900

    complete ?

---
+++ js/content.js
@@ -0,0 +1,31 @@
+var page = {
+  title: document.title,
+  url: document.location.href
+}
+
+var data = [];
+
+chrome.storage.local.get('tempmark', function(temp){
+  data = data.concat(temp.tempmark)
+})
+
+// Ctrl+Bで一時保存
+document.onkeydown = function (e) {
+  if (e.ctrlKey&&e.keyCode==66) save();
+}
+
+// 被ってるやつ削除しつつ保存
+function save() {
+  for(var i=0;i<data.length;i++) {
+    if(data[i].url==page.url)data.splice(i,1);
+  }
+  data.unshift(page)
+  if (data.length>10) {
+    data.length = 10
+  }
+  chrome.storage.local.set({'tempmark': data },function() {
+    if (chrome.runtime.error) {
+      console.log("tempmark error")
+    }
+  });
+}
--- js/popup.js
+++ js/popup.js
@@ -1,4 +1,48 @@
-$(function(){
-  var bg = chrome.extension.getBackgroundPage();
-  $('#counter').val(bg.getCounter());
-});
+var data = []
+chrome.storage.local.get('tempmark', function(temp){
+  data = data.concat(temp.tempmark)
+  for(var i=0;i<data.length;i++){
+    try {
+      var title = data[i].title
+      var url = data[i].url
+      var num = i
+
+      var tempmark = 
+        "<p id=\"mark"+num+"\">"+
+          "<a href=\""+url+"\" target=\"_blank\">"+title+"</a>"+
+          "<input id=btn"+num+" type=\"button\" value=\"x\">"+
+        "</p>";
+      $("#tempmarks").append(tempmark);
+      $("#btn"+num).on("click", function() {
+        deleteone("#mark"+num, title)
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  $("#deleteAll").on("click", deleteAll)
+})
+
+function deleteone(id, title) {
+  $(id).remove()
+  data.some(function(v,index) {
+    if(v.title==title) {
+      data.splice(index, 1)
+    }
+  });
+  save()
+}
+
+function deleteAll() {
+  data = []
+  $("#tempmarks").empty()
+  save()
+}
+
+function save() {
+  chrome.storage.local.set({'tempmark': data}, function() {
+    if (chrome.runtime.error) {
+      console.log("save error")
+    }
+  });
+}
