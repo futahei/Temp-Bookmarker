e3b16606874d9ef823f5c40ac3ed84c6ac4976b8
futahei-surface <futahei1108@icloud.com>
2018-05-15 12:43:26 +0900

    fix

--- js/content.js
+++ js/content.js
@@ -1,3 +1,33 @@
+
+var allowNotify = false
+
+function notify() {
+  if(!("Notification" in window)) {
+    return;
+  }
+
+  else if (Notification.permission === "denied") {
+    allowNotify = false
+  }
+
+  else if (Notification.permission === "granted") {
+    allowNotify = true
+    var notification = new Notification("Hi!")
+  }
+
+  else if (Notification.permission === "default") {
+    Notification.requestPermission(function(permission) {
+      if(permission==="granted") {
+        allowNotify = true
+      } else {
+        allowNotify = false
+      }
+    })
+  }
+}
+
+notify()
+
 var page = {
   title: document.title,
   url: document.location.href
@@ -16,6 +46,7 @@ document.onkeydown = function (e) {
 
 // 被ってるやつ削除しつつ保存
 function save() {
+  var not = new Notification("Save Tempmark")
   for(var i=0;i<data.length;i++) {
     if(data[i].url==page.url)data.splice(i,1);
   }
